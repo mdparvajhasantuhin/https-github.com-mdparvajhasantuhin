12459aff7586e9fa6f374602dac781dd66c0c664
Md.Parvaj Hasan Tuhin <mdparvajhasantuhin@gmail.com>
2026-09-19 22:13:38 +0600

    Delete temparecher.py

--- temparecher.py
+++
@@ -1,6 +0,0 @@
-F=int(input("Enter the Farenhit Temparecher="))
-c=int(input("Enter the Celdius Temparecher="))
-C= (F- 32) * 5 / 9
-print("Celdius Temarecher=",C)
-f= (c * 9 / 5) + 32
-print(" Farenhit Temparecher=",f)
