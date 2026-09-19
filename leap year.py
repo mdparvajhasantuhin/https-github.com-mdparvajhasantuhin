6507f223af67d7c738ce4400faa98d6fcf01c76e
Md.Parvaj Hasan Tuhin <mdparvajhasantuhin@gmail.com>
2026-09-19 22:12:35 +0600

    Delete leap year.py

--- leap year.py
+++
@@ -1,5 +0,0 @@
-year=int (input("Enter the Year:"))
-if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)):
-    print("Year is leap year",year)
-else:
-    print("Year is not leap year",year)
